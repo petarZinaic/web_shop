c4fc393d2bbdeaa0af1f0193664766bb70add1cc
petar-zinaic <petar.zinaic@marbleit.rs>
2021-09-12 14:13:07 +0200

    Add middleware, user model and user controller

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser")
 require("dotenv").config();
 
 const userRoutes = require("./routes/user");
 
 
 const app = express();
+app.use(express.json());  
 
-mongoose.connect(process.env.DATABASE)
-    .then(() => console.log("DB Connected"))  
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDb connected.'));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(cookieParser())  
 
 app.use("/api", userRoutes);    
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,16 @@
-exports.sayHi = (req, res) => {
-    res.json({message: "Hello there"});
+const User = require("../models/user");
+
+exports.signup = (req, res) => {
+    const user = new User(req.body);
+    console.log("req.body",req.body);
+    user.save((errror, user) => {
+        if(errror) {
+            return res.status(400).json({
+                errror
+            })
+        }
+        res.json({
+            user
+        })
+    });
 }
