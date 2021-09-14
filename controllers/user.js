83d4dd86b12f93d2687d0e69b72fe4972d496796
petar-zinaic <petar.zinaic@marbleit.rs>
2021-09-14 23:50:57 +0200

    Implement db error handler

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
     const user = new User(req.body);
@@ -6,9 +7,11 @@ exports.signup = (req, res) => {
     user.save((errror, user) => {
         if(errror) {
             return res.status(400).json({
-                errror
+                errror: errorHandler(errror)
             })
         }
+        user.salt = undefined;
+        user.hashed_password = undefined;
         res.json({
             user
         })
