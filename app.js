c8091edf629aa7b47033d646d6901f7eb9bf9d15
petar-zinaic <petar.zinaic@marbleit.rs>
2021-09-21 23:57:11 +0200

    Product model, controller and routes

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category")
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 
 const app = express();
@@ -29,6 +30,7 @@ app.use(expressValidator());
 app.use("/api", authRoutes);    
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api/", productRoutes);
 
 const port = process.env.PORT || 8000;
 
--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const { create } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 
  
 router.post(
-    "/category/create/:userId", 
+    "/product/create/:userId", 
     requireSignin,
     isAdmin,
     create
