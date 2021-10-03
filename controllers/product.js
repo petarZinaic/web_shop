bd7c674b5234fa40665e64fe40fa4fab8aca140d
petar-zinaic <petar.zinaic@marbleit.rs>
2021-10-03 20:49:17 +0200

    List related product

--- controllers/product.js
+++ controllers/product.js
@@ -139,7 +139,7 @@ exports.update = (req, res) => {
 exports.list = (req, res) => {
     let order = req.query.order ? req.query.order : "asc"
     let sortBy = req.query.sortBy ? req.query.sortBy : "_id"
-    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
     Product.find()
         .select("-photo")
@@ -155,10 +155,23 @@ exports.list = (req, res) => {
 
             res.send(products)
         });
-
-
 }
 
+exports.listRelated = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+    Product.find({ _id: { $ne: req.product }, category: req.product.category })
+        .limit(limit)
+        .populate('category', '_id name')
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Products not found'
+                });
+            }
+            res.json(products);
+        });
+};
 
 
 
